86f6f1c565909ab3c28c9ff22181df9e1ec43cdb
Egor Tsyganchuk <cyaneor@gmail.com>
2024-12-04 02:30:58 +0300

    Обновлена документация в runtime_error.h и runtime_error.c

--- src/runtime_error.c
+++ src/runtime_error.c
@@ -1,16 +1,11 @@
 #include <ae/runtime_error.h>
 
 /**
- * @brief Переменная для хранения кода ошибки времени выполнения.
+ * @brief Переменная для хранения кода ошибки выполнения.
  *
- * Эта переменная используется для отслеживания ошибок,
- * которые могут возникнуть в процессе выполнения программы.
- *
- * Она объявлена с использованием атрибута THREAD_LOCAL,
- * что означает, что у каждой нити будет своя собственная копия этой переменной.
- *
- * @see ae_error_code_t
- * @see AE_RUNTIME_ERROR_OK
+ * Эта переменная хранит код последней ошибки выполнения.
+ * Она объявлена как потокобезопасная (THREAD_LOCAL),
+ * что позволяет каждому потоку иметь свою собственную копию этой переменной.
  */
 AE_ATTRIBUTE(THREAD_LOCAL)
 ae_error_code_t m_runtime_error_code = AE_RUNTIME_ERROR_OK;
@@ -25,7 +20,7 @@ ae_error_code_t
 ae_runtime_error_reset_code(ae_error_code_t error_code)
 {
     const ae_error_code_t prev = m_runtime_error_code;
-    m_runtime_error_code = error_code;
+    m_runtime_error_code       = error_code;
     return prev;
 }
 
