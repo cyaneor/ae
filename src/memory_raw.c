13afc24b72d33cb8ca2aeaa2fb7786f3c2081d7f
Egor Tsyganchuk <cyaneor@gmail.com>
2025-02-15 21:41:04 +0300

    Исправлена ошибка в memory_raw.c в функции поиска значения.

--- src/memory_raw.c
+++ src/memory_raw.c
@@ -734,23 +734,23 @@ ae_memory_raw_find_rev_u64(const ae_u64_t *lhs,
 const ae_u8_t *
 ae_memory_raw_find_value_u8(const ae_u8_t *begin, const ae_u8_t *end, ae_u8_t value)
 {
-    return ae_memory_raw_find_u8(begin, end, &value, &value + 1);
+    return ae_memory_raw_find_u8(begin, end, &value, ae_ptr_add_offset(&value, sizeof(ae_u8_t)));
 }
 
 const ae_u16_t *
 ae_memory_raw_find_value_u16(const ae_u16_t *begin, const ae_u16_t *end, ae_u16_t value)
 {
-    return ae_memory_raw_find_u16(begin, end, &value, &value + 1);
+    return ae_memory_raw_find_u16(begin, end, &value, ae_ptr_add_offset(&value, sizeof(ae_u16_t)));
 }
 
 const ae_u32_t *
 ae_memory_raw_find_value_u32(const ae_u32_t *begin, const ae_u32_t *end, ae_u32_t value)
 {
-    return ae_memory_raw_find_u32(begin, end, &value, &value + 1);
+    return ae_memory_raw_find_u32(begin, end, &value, ae_ptr_add_offset(&value, sizeof(ae_u32_t)));
 }
 
 const ae_u64_t *
 ae_memory_raw_find_value_u64(const ae_u64_t *begin, const ae_u64_t *end, ae_u64_t value)
 {
-    return ae_memory_raw_find_u64(begin, end, &value, &value + 1);
+    return ae_memory_raw_find_u64(begin, end, &value, ae_ptr_add_offset(&value, sizeof(ae_u64_t)));
 }
