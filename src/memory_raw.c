1cba505c35abbaecb30697cb33f75a6aa684e0b5
Egor Tsyganchuk <cyaneor@gmail.com>
2025-03-02 21:41:46 +0300

    Revert "Отделены обобщенные функции от memory_raw.h"
    This reverts commit 1ce90b1270e717e8f9b80da0aea0a2dd121de306.

--- src/memory_raw.c
+++ src/memory_raw.c
@@ -59,6 +59,27 @@ ae_memory_raw_fill_u64(ae_u64_t *begin, const ae_u64_t *end, ae_u64_t value)
     return ptr;
 }
 
+void *
+ae_memory_raw_fill(void *begin, const void *end, ae_u8_t value)
+{
+    if (ae_ptr_range_is_aligned(begin, end, AE_U64_T_SIZE))
+    {
+        const ae_u64_t _value = ae_numeric_repeat64(value);
+        return ae_memory_raw_fill_u64(begin, end, _value);
+    }
+    elif (ae_ptr_range_is_aligned(begin, end, AE_U32_T_SIZE))
+    {
+        const ae_u32_t _value = ae_numeric_repeat32(value);
+        return ae_memory_raw_fill_u32(begin, end, _value);
+    }
+    elif (ae_ptr_range_is_aligned(begin, end, AE_U16_T_SIZE))
+    {
+        const ae_u16_t _value = ae_numeric_repeat16(value);
+        return ae_memory_raw_fill_u16(begin, end, _value);
+    }
+    return ae_memory_raw_fill_u8(begin, end, value);
+}
+
 const ae_u8_t *
 ae_memory_raw_compare_u8(const ae_u8_t *lhs,
                          const ae_u8_t *lhs_end,
@@ -303,6 +324,24 @@ ae_memory_raw_copy_u64(ae_u64_t       *dst,
     return _dst;
 }
 
+void *
+ae_memory_raw_copy(void *dst, const void *dst_end, const void *src, const void *src_end)
+{
+    if (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U64_T_SIZE))
+    {
+        return ae_memory_raw_copy_u64(dst, dst_end, src, src_end);
+    }
+    elif (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U32_T_SIZE))
+    {
+        return ae_memory_raw_copy_u32(dst, dst_end, src, src_end);
+    }
+    elif (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U16_T_SIZE))
+    {
+        return ae_memory_raw_copy_u16(dst, dst_end, src, src_end);
+    }
+    return ae_memory_raw_copy_u8(dst, dst_end, src, src_end);
+}
+
 ae_u8_t *
 ae_memory_raw_copy_rev_u8(const ae_u8_t *dst,
                           ae_u8_t       *dst_end,
@@ -375,6 +414,24 @@ ae_memory_raw_copy_rev_u64(const ae_u64_t *dst,
     return _dst;
 }
 
+void *
+ae_memory_raw_copy_rev(const void *dst, void *dst_end, const void *src, const void *src_end)
+{
+    if (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U64_T_SIZE))
+    {
+        return ae_memory_raw_copy_rev_u64(dst, dst_end, src, src_end);
+    }
+    elif (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U32_T_SIZE))
+    {
+        return ae_memory_raw_copy_rev_u32(dst, dst_end, src, src_end);
+    }
+    elif (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U16_T_SIZE))
+    {
+        return ae_memory_raw_copy_rev_u16(dst, dst_end, src, src_end);
+    }
+    return ae_memory_raw_copy_rev_u8(dst, dst_end, src, src_end);
+}
+
 ae_u8_t *
 ae_memory_raw_move_u8(ae_u8_t *dst, ae_u8_t *dst_end, const ae_u8_t *src, const ae_u8_t *src_end)
 {
@@ -428,6 +485,23 @@ ae_memory_raw_move_u64(ae_u64_t       *dst,
     return ae_memory_raw_copy_u64(dst, dst_end, src, src_end);
 }
 
+void *
+ae_memory_raw_move(void *dst, void *dst_end, const void *src, const void *src_end)
+{
+    if (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U64_T_SIZE))
+    {
+        return ae_memory_raw_move_u64(dst, dst_end, src, src_end);
+    }
+    elif (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U32_T_SIZE))
+    {
+        return ae_memory_raw_move_u32(dst, dst_end, src, src_end);
+    }
+    elif (ae_ptr_range_is_aligned_both(dst, dst_end, src, src_end, AE_U16_T_SIZE))
+    {
+        return ae_memory_raw_move_u16(dst, dst_end, src, src_end);
+    }
+    return ae_memory_raw_move_u8(dst, dst_end, src, src_end);
+}
 
 const ae_u8_t *
 ae_memory_raw_find_u8(const ae_u8_t *lhs,
@@ -651,4 +725,19 @@ ae_memory_raw_swap_u64(ae_u64_t *lhs, ae_u64_t *rhs)
 {
     AE_RUNTIME_ASSERT(lhs && rhs, AE_RUNTIME_ERROR_NULL_POINTER);
     ae_memory_raw_swap(*lhs, *rhs);
+}
+
+void *
+ae_memory_raw_shift_left(void *dst, void *dst_end, ae_usize_t shift)
+{
+    void *_ptr = ae_ptr_add_offset(dst, shift);
+    return ae_memory_raw_move(dst, dst_end, _ptr, dst_end);
+}
+
+void *
+ae_memory_raw_shift_right(void *dst, void *dst_end, ae_usize_t shift)
+{
+    void *_ptr = ae_ptr_add_offset(dst, shift);
+    void *_end = ae_ptr_sub_offset(dst_end, shift);
+    return ae_memory_raw_move(_ptr, dst_end, dst, _end);
 }
