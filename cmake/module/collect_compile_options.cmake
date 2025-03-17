087455cdeb5a5b43f56e00adf595b95da2e33ce5
Egor Tsyganchuk <cyaneor@gmail.com>
2025-03-17 11:20:01 +0300

    Добавлена опция сборки в отладочном режиме

--- cmake/module/collect_compile_options.cmake
+++ cmake/module/collect_compile_options.cmake
@@ -11,95 +11,131 @@ get_property(CMAKE_OPTIONS DIRECTORY PROPERTY VARIABLES)
 
 message(STATUS "Listing all enabled compile options:")
 foreach (CMAKE_OPTION IN ITEMS ${CMAKE_OPTIONS})
-    if (CMAKE_OPTION MATCHES "^AE_COMPILE_OPTION_.*" AND NOT ${CMAKE_OPTION} STREQUAL "OFF")
-        # Печатаем ключ и значение опции с дополнительным форматированием
-        message(STATUS ":: ${CMAKE_OPTION}")
+    if (CMAKE_OPTION MATCHES "^AE_COMPILE_OPTION_.*")
+        if (${CMAKE_OPTION} STREQUAL "ON")
+            message(STATUS ":: ${CMAKE_OPTION}")
+        endif ()
 
         # Для GCC, Clang или MSVC
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_OPTIMIZATION")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -O3)
-            elseif (MSVC)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /O2)  # MSVC аналог -O3
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -O3)
+                elseif (MSVC)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /O2)  # MSVC аналог -O3
+                endif ()
+            else ()
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -O0)  # Для GCC и Clang отключение оптимизаций
+                elseif (MSVC)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /Od)
+                endif ()
+            endif ()
+            continue()
+        endif ()
+
+        if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_DEBUG")
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -g)  # Для GCC и Clang включение флага отладки
+                elseif (MSVC)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /Zi)  # Для MSVC включение флага отладки
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_SSE3")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -msse3)
-            elseif (MSVC)
-                # Для MSVC флаг /arch:SSE2 включает поддержку SSE3,
-                # так как SSE3 является частью SSE2 и не требует отдельного флага.
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /arch:SSE2)  # Вместо /arch:SSE3
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -msse3)
+                elseif (MSVC)
+                    # Для MSVC флаг /arch:SSE2 включает поддержку SSE3,
+                    # так как SSE3 является частью SSE2 и не требует отдельного флага.
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /arch:SSE2)  # Вместо /arch:SSE3
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_LTO")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -flto)
-            elseif (MSVC)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /GL)  # MSVC аналог LTO
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -flto)
+                elseif (MSVC)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /GL)  # MSVC аналог LTO
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_UNROLL_LOOPS")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -funroll-loops)
-            elseif (MSVC)
-                # MSVC не поддерживает /unroll.
-                # Однако компилятор сам обычно развертывает циклы, если оптимизация включена.
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -funroll-loops)
+                elseif (MSVC)
+                    # MSVC не поддерживает /unroll.
+                    # Однако компилятор сам обычно развертывает циклы, если оптимизация включена.
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_NO_STACK_PROTECTOR")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fno-stack-protector)
-            elseif (MSVC)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /GS-)  # MSVC аналог -fno-stack-protector
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fno-stack-protector)
+                elseif (MSVC)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /GS-)  # MSVC аналог -fno-stack-protector
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_PIC")
-            if (${AE_OPTION_STATIC_BUILD})
-                message(FATAL_ERROR "PIC (Position Independent Code) cannot be used in a static build.")
-            endif ()
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (${AE_OPTION_STATIC_BUILD})
+                    message(FATAL_ERROR "PIC (Position Independent Code) cannot be used in a static build.")
+                endif ()
 
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fPIC)
-            elseif (MSVC)
-                # MSVC не поддерживает флаг PIC, код всегда компилируется с позиционно независимым адресом
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fPIC)
+                elseif (MSVC)
+                    # MSVC не поддерживает флаг PIC, код всегда компилируется с позиционно независимым адресом
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_FNO_RTTI")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fno-rtti)
-            elseif (MSVC)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /GR-)  # MSVC аналог -fno-rtti
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fno-rtti)
+                elseif (MSVC)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /GR-)  # MSVC аналог -fno-rtti
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_FNO_EXCEPTIONS")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fno-exceptions)
-            elseif (MSVC)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /EHsc-)  # MSVC аналог -fno-exceptions
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -fno-exceptions)
+                elseif (MSVC)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS /EHsc-)  # MSVC аналог -fno-exceptions
+                endif ()
             endif ()
             continue()
         endif ()
 
         if (CMAKE_OPTION STREQUAL "AE_COMPILE_OPTION_THREAD_SAFETY")
-            if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
-                list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -pthread)
-            elseif (MSVC)
-                # MSVC использует другие механизмы для потокобезопасности, не требуется параметр
+            if (${CMAKE_OPTION} STREQUAL "ON")
+                if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_CLANG)
+                    list(APPEND AE_TARGET_PRIVATE_COMPILE_OPTIONS -pthread)
+                elseif (MSVC)
+                    # MSVC использует другие механизмы для потокобезопасности, не требуется параметр
+                endif ()
             endif ()
             continue()
         endif ()
