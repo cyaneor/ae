2619f384446878b7460e97d8903ba217915c249d
Egor Tsyganchuk <cyaneor@gmail.com>
2025-03-17 22:59:05 +0300

    Добавлен linux-cmake-build.sh для автоматической сборки под linux

--- mingw-cmake-build.cmd
+++ mingw-cmake-build.cmd
@@ -1,7 +1,7 @@
 @echo off
 :: Check if the mode (Debug or Release) is provided as a parameter
 if "%1"=="" (
-    echo No build type specified. Please use "Debug" or "Release".
+    echo No build type specified. Please use "debug" or "release".
     echo Usage: build.cmd [debug|release]
     pause
     exit /b
@@ -10,7 +10,7 @@ if "%1"=="" (
 :: Set the project directory path, change if needed
 set PROJECT_DIR=%cd%
 
-:: Set the build directory based on the input parameter (Debug or Release)
+:: Set the build directory based on the input parameter (debug or release)
 set BUILD_DIR=%PROJECT_DIR%\cmake-build-%1
 
 :: Delete the build directory if it exists
@@ -26,7 +26,7 @@ mkdir %BUILD_DIR%
 :: Change to the build directory
 cd %BUILD_DIR%
 
-:: Run CMake with the necessary parameters based on the selected mode (Debug or Release)
+:: Run CMake with the necessary parameters based on the selected mode (debug or release)
 echo Running CMake for %1...
 cmake .. -DCMAKE_BUILD_TYPE=%1 -G "CodeBlocks - MinGW Makefiles"
 
